5f7b0a6aeb2e5431e2602baa4e0d92101dd0debe
luis <lfdasilva9@gmail.com>
2021-01-21 20:01:03 -0300

    subindo o codigo

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// function soma (n1 , n2){
+//     return n1+n2;
+// }
+// function validaIdade(idade){
+//     if(idade >= 18){
+//         var validar = true;
+//     }else{
+//         validar = false;
+//     }
+//     return validar;
+   
+
+// }
+
+// var idade = prompt("Qual e sua idade?")
+
+function button(){
+
+    document.getElementById("obg").innerHTML = "click teste";
+
+    // alert("teste")
+}
+
+
